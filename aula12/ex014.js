ddbececa0e51f0074fc0b40e44fe94dac2c0b261
chrebelo <119673038+chrebelo@users.noreply.github.com>
2024-03-25 15:58:06 -0300

    Create ex014.js

---
+++ aula12/ex014.js
@@ -0,0 +1,21 @@
+var diaatual = new Date()
+var dia = diaatual.getDay()
+var dia = 4;
+console.log(`Hoje é ${dia}`)
+if (dia = 0) {
+    console.log('Domingo')
+} else if (dia = 1) {
+    console.log('Segunda')
+} else if (dia = 2) {
+    console.log('Terça')
+} else if (dia = 3) {
+    console.log('Quarta')
+} else if (dia = 4) {
+    console.log('Quinta')
+} else if (dia = 5) {
+    console.log('Sexta')
+} else if (dia = 6) {
+    console.log('Sábado')
+} else {
+    console.log('[ERRO] Dia inválido')
+}

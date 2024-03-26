570413d042cd85a2d0a1ed86bd9c4327b3f40fde
chrebelo <119673038+chrebelo@users.noreply.github.com>
2024-03-26 16:40:15 -0300

    update ex015

--- aula12ex/ex015/scripts/script.js
+++ aula12ex/ex015/scripts/script.js
@@ -1,3 +1,13 @@
-function carregar() {
-    
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano) > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
+    }
 }

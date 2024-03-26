dc3813bb8f0ba1b6713308f83097ef52628e6aaa
chrebelo <119673038+chrebelo@users.noreply.github.com>
2024-03-26 15:43:38 -0300

    update ex014

--- aula12ex/ex014/scripts/script.js
+++ aula12ex/ex014/scripts/script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas!`
     if (hora > 0 && hora < 12) {
         // Bom dia
-        img.src = 'manha.png'
+        img.src = 'imagens/manha.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde
-        img.src = 'tarde.png'
+        img.src = 'imagens/tarde.png'
         document.body.style.background = '#b9846f'
     } else {
         // Boa noite
-        img.src = 'noite.png'
+        img.src = 'imagens/noite.png'
         document.body.style.background = '#515154'
     }
 }

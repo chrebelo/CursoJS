35d19ecd22da617b3e2366296019f398fc070b1b
chrebelo <119673038+chrebelo@users.noreply.github.com>
2024-04-09 16:27:18 -0300

    update ex017

---
+++ aula14ex/ex017/scripts/script.js
@@ -0,0 +1,11 @@
+function tabuada() {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Digite um número!')
+        
+    } else {
+        var n = Number(num.value)
+    }
+    
+}

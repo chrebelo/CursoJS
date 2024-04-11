624b3b55b40dff235fa0c5f11e7d9ccd7c3cecae
chrebelo <119673038+chrebelo@users.noreply.github.com>
2024-04-11 10:11:35 -0300

    update ex017

--- aula14ex/ex017/scripts/script.js
+++ aula14ex/ex017/scripts/script.js
@@ -6,6 +6,15 @@ function tabuada() {
         
     } else {
         var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
     
 }
